91e0c12f43a231e2479ba3a76f1c49848daabe02
Gianfranco <gianfrancomalzone.gm@gmail.com>
2022-08-12 00:05:14 -0300

    act 1.0

---
+++ js/index.js
@@ -0,0 +1,87 @@
+
+
+
+function datosProdutos(
+  precioNetoProducto,
+  porcentajeImpuestos,
+  porcentajeGanarcia,
+) {
+  let precioCF = precioNetoProducto*((porcentajeImpuestos/100)+(1))*((porcentajeGanarcia/100)+1) ;
+  alert(precioCF);
+}
+let precioCF = datosProdutos (+prompt("Ingrese el precio neto del producto"),+prompt("Ingrese el porcentaje de impuestos a pagar por el producto"),+prompt("Ingrese el porcentaje de ganancia que quiere obtener con el producto"));
+
+// datosProdutos (+prompt("Ingrese el precio neto del producto"),+prompt("Ingrese el porcentaje de impuestos a pagar por el producto"),+prompt("Ingrese el porcentaje de ganancia que quiere obtener con el producto"));
+
+function producto(nombreProducto, precioVenta, stockProducto, stockVendido) {
+    this.nombreProducto = nombreProducto;
+    this.precioVenta = precioVenta;
+    this.stockProducto = stockProducto;
+    this.stockVendido = stockVendido; 
+}
+
+const producto1 = new producto ("Latex 1L", precioCF, 10, 5)
+
+console.log (producto1);
+
+const listaProductosFaltantes = [];
+let cantidadProductosFaltantes = +prompt("Ingresa la cantidad de productos faltantes.");
+
+do {
+  let entrada = prompt("Ingresa los nombres de los productos faltantes.") ;
+  listaProductosFaltantes.push(entrada.toUpperCase());
+  console.log(listaProductosFaltantes.length);
+}while(listaProductosFaltantes.length != cantidadProductosFaltantes);
+
+console.log(listaProductosFaltantes.join("\n"));
+
+const produtosCargados = [
+  {nombre: `Latex 1L`, precio: `precio`},
+  {nombre: ``, precio: ``},
+]
+
+const buscarNombre = produtosCargados.find((el) => el.nombre === "Latex 1L")
+const buscarPrecio = produtosCargados.find((el) => el.precio === "400")
+
+console.log(buscarNombre) ;
+
+console.log(buscarPrecio) ;
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
